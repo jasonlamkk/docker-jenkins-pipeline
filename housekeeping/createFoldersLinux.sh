ebd1b5e871a2c2d52821dffe2c1dbbd7bfa641d2
jasonlam <jlam@palo-it.com>
2019-02-15 09:51:05 +0800

    Add visual output Rename Mac script with the word mac

--- housekeeping/createFoldersLinux.sh
+++ housekeeping/createFoldersLinux.sh
@@ -8,15 +8,20 @@ else
 fi
 
 sudo mkdir -p ${DATA_FOLDER}/documents
+echo "Created documents folder for Jenkins on : ${DATA_FOLDER}/documents"
 
 sudo mkdir -p ${DATA_FOLDER}/home
+echo "Created home folder for Jenkins on : ${DATA_FOLDER}/home"
 
 sudo mkdir -p ${DATA_FOLDER}/.ssh
+echo "Created SSH configuration folder for Jenkins on : ${DATA_FOLDER}/.ssh"
 
 sudo chown -R $(whoami) ${DATA_FOLDER}
+echo "Changed the owner of Jenkins folders to $(whoami)"
 
 sudo mkdir -p ${DOCKER_HOST_BASE}/selenium/firefox/shm
-
 sudo mkdir -p ${DOCKER_HOST_BASE}/selenium/chrome/shm
+echo "Created folders for selenium on ${DOCKER_HOST_BASE}/selenium "
 
 sudo chown -R $(whoami) ${DOCKER_HOST_BASE}/selenium
+echo "Changed the owner of selenium folders to $(whoami)"
